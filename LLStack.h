a3220241febc23cf539ee685a147ccc5a1267beb
anvari1313 <ahmad.anv@gmail.com>
2016-11-27 16:23:40 +0330

    change size function to getsize and add last function

--- LLStack.h
+++ LLStack.h
@@ -7,16 +7,28 @@
 #ifndef STACK_LLSTACK_H
 #define STACK_LLSTACK_H
 
+#define nullptr 0
+
+template <class type>
+class LLStackNode
+{
+public:
+    type data;
+    LLStackNode *last;
+};
 
 template <class type> class LLStack {
 private:
+    long size;
+    LLStackNode <type>*head;
 
 public:
     LLStack();
     void push(type data);
-    type pop();
+    type *pop(type &data);
+    type last();
     bool isEmpty();
-    long size();
+    long getSize();
 };
 
 #include "LLStack.cpp"
